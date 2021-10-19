5004d767e62ad755bcf65d1b353155e788dc6c51
DanielCela111 <danielcela111@gmail.com>
2021-10-19 23:58:04 +0200

    Dita 1

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.style.scss';
+import Directory from '../../components/menu-item/directory/directory.component';
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <Directory />
+        </div>
+    );
+};
+
+export default HomePage;
