06e6113bc5ed6d81854e5f590188f3a1f80b4e0c
DanielCela111 <danielcela111@gmail.com>
2021-11-03 23:41:43 +0100

    Moved Directory State Into Redux newww

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./homepage.style.scss";
-import Directory from "../../components/menu-item/directory/directory.component";
+import Directory from "../../components/directory/directory.component";
 
 const HomePage = () => {
   return (
