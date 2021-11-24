e08dc0975e33ea7035d1594b58d78522c145190e
DanielCela111 <danielcela111@gmail.com>
2021-11-24 23:48:03 +0100

    Revert "Revert "Revert "Asynchronous Redux better refactor and faster code"""
    This reverts commit af5cc1e8931c4fae49aac243712e078b684baaaa.

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -16,13 +16,3 @@ export const selectCollection = (collectionUrlParam) =>
   createSelector([selectCollections], (collections) =>
     collections ? collections[collectionUrlParam] : null
   );
-
-export const selectIsCollectionFetching = createSelector(
-  [selectShop],
-  (shop) => shop.isFetching
-);
-
-export const selectIsCollectionsLoaded = createSelector(
-  [selectShop],
-  (shop) => !!shop.collections
-);
