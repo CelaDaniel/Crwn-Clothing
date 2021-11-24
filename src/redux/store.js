28febf1648f66c986b1205dcf84f201d370f451b
DanielCela111 <danielcela111@gmail.com>
2021-11-24 23:41:51 +0100

    Revert "Asynchronous Redux better refactor and faster code"
    This reverts commit 130ef6fac3b4e6e608f1a3851dfa10392aa8974a.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import {persistStore} from "redux-persist";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
 import  rootReducer from "./root-reducer";
 
-const middlewares = [thunk];
+const middlewares = [];
 if(process.env.NODE_ENV === "development") {
     middlewares.push(logger)
 }
